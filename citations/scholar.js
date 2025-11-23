b1169473d9b305b0a4db58cc88aa47dcf6343f92
voidism <b05901033@ntu.edu.tw>
2025-11-23 15:39:10 -0500

    add citations numbers realtime

--- citations/scholar.js
+++
@@ -1,6 +0,0 @@
-const citationData = {
-    "citations": 3046,
-    "updated_by": "local-scrape",
-    "source": "https://scholar.google.com/citations?user=3ar1DOwAAAAJ&hl=en",
-    "date": "2025-08-02T23:35:02Z"
-};
